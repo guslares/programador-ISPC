78dc3fb2335cc6b45ae0c41dcd5a70c3fb30885c
Barbara <batissera@gmail.com>
2022-08-13 18:22:03 -0300

    subo ejercicio 5 de Python

---
+++ ejercicio 5.py
@@ -0,0 +1,16 @@
+
+historial = (2350, 5960, 23000, 1000,8900)
+
+print(historial)
+
+suma=sum(historial)
+
+if suma < 30000:
+    print ("El monto total por la atencion de Pupy es: ", suma)
+else:
+    print ("Gastos por encima de lo presupuestado")
+
+
+
+
+ 
